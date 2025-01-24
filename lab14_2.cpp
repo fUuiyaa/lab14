aeb35b1fbd4d0780c6eac3f64e806b0fc5338520
fUuiyaa <kengwanitchitsanupong@gmail.com>
2025-01-24 16:12:54 +0700

    lab14_2 completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,39 @@ int main(){
 }
 
 //Do not modify source code above this line 
+
+void showMatrix(const bool x[][N]){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			cout << x[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void inputMatrix(double x[][N]){
+	for(int i=0;i<N;i++){
+		cout << "Row " << i+1 << ": ";
+		for(int j=0;j<N;j++){
+			cin >> x[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double x[][N], bool y[][N]){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			int tem1 = i-1, tem2 = j-1;
+			int temp1=i+1, temp2=j+1;
+		
+			if(tem1<0||tem2<0||temp1>=N||temp2>=N){
+				y[i][j]=0;
+			} else if(x[i][j]>=x[i+1][j] && x[i][j]>=x[i][j+1] && x[i][j]>=x[i-1][j] && x[i][j]>=x[i][j-1]){
+					y[i][j]=1;
+			} else{
+				y[i][j]=0;
+			}
+
+		}
+	}
+}
